63809a8a4a2aadb50109b4e5195b84454022d0e5
Dimas Kiperman <126020343+kiper7777@users.noreply.github.com>
2025-05-14 14:53:09 +0100

    Add files via upload

---
+++ w1_13.05.25/prog6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class prog6 {
+    public static void main(String[] args) {
+        byte x=10;
+        short y=12;
+        int z=122;
+        long v=1234;
+
+        System.out.println("the value of x is " + x);
+        Scanner sc=new Scanner(System.in);
+        // System.out.println("Enter the value");
+        // int num=sc.nextInt();
+        // System.out.println("the value of num is " + num);
+        // float b=sc.nextFloat();
+        // System.out.println(b);
+        String name=sc.nextLine();
+        System.out.println(name);
+    }
+}
